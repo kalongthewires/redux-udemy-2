4a8c5e33ef7f4b210b259ae32156322868b7bfa0
Kim Hamper <kim.hamper@gmail.com>
2016-07-26 09:26:03 -0400

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Stuff', pages: 1 },
+        { title: 'Stuff2', pages: 2 },
+        { title: 'Stuff3', pages: 3 },
+        { title: 'Stuff4', pages: 4 }
+    ];
+}
